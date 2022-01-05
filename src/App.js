7d49451b460e24684115f933e3886108598f7049
rusty-yuri <rustywhite404@gmail.com>
2022-01-05 22:42:49 +0900

    220105 : Challenge 02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
 
+    return (
+      <div>
+        <h1>START REACT 200</h1>
+        <p>HTML 적용하기</p>
+      </div>
+    );
+
+}
 export default App;
